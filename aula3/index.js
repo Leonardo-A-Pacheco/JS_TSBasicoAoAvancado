fdf3405f1f61447d2068bf95646cf6117c118d8b
Leonardo-A-Pacheco <l00000111.pacheco@gmail.com>
2023-12-27 16:20:50 -0300

    refresh-repository

---
+++ aula3/index.js
@@ -0,0 +1,26 @@
+console.log('bla');
+console.log("bla");
+// template string
+console.log(`'bla' "bla"`);
+
+
+// tudo e number em js 32 é a mesma tipagem de 32.5
+
+console.log(1234, 15.85, 'valor');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
