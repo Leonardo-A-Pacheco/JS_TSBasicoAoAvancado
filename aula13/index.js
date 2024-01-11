96b91dfe66a11a1db181daa4ef7a7856a66e9cf4
Leonardo-A-Pacheco <l00000111.pacheco@gmail.com>
2024-01-11 02:37:18 -0300

    24 mais sobre strings

---
+++ aula13/index.js
@@ -0,0 +1,48 @@
+//as strings são indexaveis
+//               0123456789...
+let umaString = 'um "textinho"';
+
+//se usar a barra invertida \ funciona como escape
+console.log(umaString);
+
+console.log(umaString[4]);
+
+console.log(umaString.charAt(6));
+
+
+console.log(`${umaString} concatenando tudo`);
+// ou 
+console.log(umaString + ' concatenando tudo');
+//ou
+console.log(umaString.concat(' concatenando tudo'));
+
+//descobrir o indice da palavra textinho
+
+console.log(umaString.indexOf('textinho', 0));
+// buscando de tras para frente
+console.log(umaString.lastIndexOf('um', 5));
+//trocando valores
+console.log(umaString.replace('um', 'unico'));
+//tamanho total da string
+console.log(umaString.length);
+
+//                 0123456789
+let outraString = 'cavalos são pessoas grandes';
+
+// separando por indice
+console.log(outraString.slice(8, 11));
+console.log(outraString.slice(-7));
+
+// separando string conforme caractere
+console.log(outraString.split(' '));
+console.log(outraString.toLocaleUpperCase());
+console.log(outraString.toLocaleLowerCase());
+
+
+
+
+
+
+
+
+
